8ad489ddd05eb77905a4e7a4b0359eb17c4e3834
exelseries <anichukwufaithful@gmail.com>
2026-08-04 03:49:16 +0100

    Update footer branding to Powered by Jexel Core Technologies (www.jexeltech.com)

--- packages/Webkul/Admin/src/Resources/lang/en/app.php
+++ packages/Webkul/Admin/src/Resources/lang/en/app.php
@@ -6,7 +6,7 @@ return [
             'email' => 'Email Address',
             'forget-password-link' => 'Forget Password ?',
             'password' => 'Password',
-            'powered-by-description' => 'Powered by :bagisto, an open-source project by :webkul.',
+            'powered-by-description' => 'Powered by <a class="text-[#C5A059] font-semibold hover:underline" href="https://www.jexeltech.com" target="_blank">Jexel Core Technologies</a>',
             'submit-btn' => 'Sign In',
             'title' => 'Sign In',
         ],
@@ -16,7 +16,7 @@ return [
                 'email' => 'Registered Email',
                 'email-not-exist' => 'Email Not Exists',
                 'page-title' => 'Forget Password',
-                'powered-by-description' => 'Powered by :bagisto, an open-source project by :webkul.',
+                'powered-by-description' => 'Powered by <a class="text-[#C5A059] font-semibold hover:underline" href="https://www.jexeltech.com" target="_blank">Jexel Core Technologies</a>',
                 'reset-link-sent' => 'Reset Password link sent',
                 'sign-in-link' => 'Back to Sign In ?',
                 'submit-btn' => 'Reset',
@@ -29,7 +29,7 @@ return [
             'confirm-password' => 'Confirm Password',
             'email' => 'Registered Email',
             'password' => 'Password',
-            'powered-by-description' => 'Powered by :bagisto, an open-source project by :webkul.',
+            'powered-by-description' => 'Powered by <a class="text-[#C5A059] font-semibold hover:underline" href="https://www.jexeltech.com" target="_blank">Jexel Core Technologies</a>',
             'submit-btn' => 'Reset Password',
             'title' => 'Reset Password',
         ],
@@ -5339,7 +5339,7 @@ return [
             ],
 
             'powered-by' => [
-                'description' => 'Powered by :bagisto, an open-source project by :webkul.',
+                'description' => 'Powered by <a class="text-[#C5A059] font-semibold hover:underline" href="https://www.jexeltech.com" target="_blank">Jexel Core Technologies</a>',
             ],
         ],
 
